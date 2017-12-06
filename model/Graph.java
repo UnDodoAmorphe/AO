ffa740f9578ed271d08ccef6bd50b253f6d5b1a9
UnDodoAmorphe <flo.gautier10@gmail.com>
2017-12-06 10:10:08 +0100

    Changements du prof

---
+++ model/Graph.java
@@ -0,0 +1,38 @@
+package model;
+import org.jgrapht.graph.SimpleGraph;
+
+import model.Labyrinthe.Directions;
+
+@SuppressWarnings("serial")
+public class Graph extends SimpleGraph<Sommet, Arc> {
+
+	public Graph(Class<? extends Arc> edgeClass) {
+		super(edgeClass);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static boolean doesntExist(Sommet vertex, Directions dir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Sommet randomVertex() {
+		//myVertexes = this.vertexSet(); -> puis parcourir puis en choisisr un random ?
+		return null;
+	}
+
+	public Arc randomEdge() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Arc getEdge(Sommet vertex, Directions dir) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Sommet getVertexByDir(Sommet vertex, Directions dir) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ model/Sommet.java
@@ -0,0 +1,82 @@
+package model;
+
+import model.Labyrinthe.Directions;
+
+public class Sommet implements Comparable<Sommet>{
+private int x,y,nbr;
+private boolean containCandy;
+private boolean containButton;
+
+public Sommet(int x, int y) {
+	this.x=x;
+	this.y=y;
+
+	this.containCandy=false;
+	this.containButton=false;
+
+}
+
+public Sommet () {
+
+}
+
+@Override
+public int compareTo(Sommet o) {
+	if (x != o.x)
+		return o.x-x;
+	else
+		return o.y-y;
+ 	}
+
+public boolean inBorders(Directions dir) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+public int getX() {
+	// TODO Auto-generated method stub
+	return x;
+}
+
+public int getY() {
+	// TODO Auto-generated method stub
+	return y;
+}
+
+public int getNbr() {
+	// TODO Auto-generated method stub
+	return nbr;
+}
+public boolean setCandy() {
+	if ((isButton()==true) || (isCandy()==true)) {
+		return false;
+	}
+	else {
+		Candy myCandy = new Candy();
+		myCandy.addCandy(this);
+		this.containCandy = true;
+		return true;
+	}
+}
+public boolean isCandy() {
+	return this.containCandy;
+}
+public boolean setButton() {
+	if ((isButton()==true) || (isCandy()==true)) {
+		return false;
+	}
+	else {
+		Button myButton = new Button();
+		myButton.addButton(this);
+		this.containButton = true;
+		return true;
+	}
+}
+public boolean isButton() {
+	return this.containButton;
+}
+
+public void setNbr(int nbr) {
+	this.nbr = nbr;
+}
+}
