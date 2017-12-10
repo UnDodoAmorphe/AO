64c774f434af84137762e7263d8d7e89a0d376af
Nasthael <vtrj.dejonghe@gmail.com>
2017-12-10 21:19:54 +0100

    il en manquait un !

--- labyrinthe/src/view/Button.java
+++ labyrinthe/src/view/Button.java
@@ -17,16 +17,16 @@ public class Button extends A_Static {
 	Image imageButtonClose= new Image(getClass().getResource("button_close.png").toExternalForm());
 	imageView= new ImageView(image);
 	Frame.pane.getChildren().add(this.imageView);
-	double xt=(int)((ViewFrame.WALL+sprite.getX()*(ViewFrame.WALL+ViewFrame.CELL))*ViewFrame.SPAN);
-	double yt=(int)((ViewFrame.WALL+sprite.getY()*(ViewFrame.WALL+ViewFrame.CELL))*ViewFrame.SPAN);
+	double xt_Close=(int)((ViewFrame.WALL+sprite.getX()*(ViewFrame.WALL+ViewFrame.CELL))*ViewFrame.SPAN);
+	double yt_Close=(int)((ViewFrame.WALL+sprite.getY()*(ViewFrame.WALL+ViewFrame.CELL))*ViewFrame.SPAN);
 	imageView.setX(xt);
 	imageView.setY(yt);
 }
 	Image imageButtonOpen= new Image(getClass().getResource("button_open.png").toExternalForm());
 	imageView= new ImageView(image);
 	Frame.pane.getChildren().add(this.imageView);
-	double xt=(int)((ViewFrame.WALL+sprite.getX()*(ViewFrame.WALL+ViewFrame.CELL))*ViewFrame.SPAN);
-	double yt=(int)((ViewFrame.WALL+sprite.getY()*(ViewFrame.WALL+ViewFrame.CELL))*ViewFrame.SPAN);
+	double xt_Open=(int)((ViewFrame.WALL+sprite.getX()*(ViewFrame.WALL+ViewFrame.CELL))*ViewFrame.SPAN);
+	double yt_Open=(int)((ViewFrame.WALL+sprite.getY()*(ViewFrame.WALL+ViewFrame.CELL))*ViewFrame.SPAN);
 	imageView.setX(xt);
 	imageView.setY(yt);
 	}
