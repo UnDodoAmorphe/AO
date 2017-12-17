2f2cba9ad87c8fd4591a587e8bf61ecb4d8925f3
Nasthael <vtrj.dejonghe@gmail.com>
2017-12-17 12:58:42 +0100

    Et ca continue : ajout de classe "TheDirector" pour la gestion du jeu et implémentation de la classe Candy. Certaines parties a adapter

--- labyrinthe/src/model/Candy.java
+++ labyrinthe/src/model/Candy.java
@@ -1,24 +1,31 @@
 package model;
+
+import java.util.Random;
+
+import controller.TheDirector;
+
 // impl�menter "est ce qu'il reste des candies dans le graph"
-public class Candy{
-int x, y;
+public class Candy extends GameObject{
+    public Candy(int x, int y) {
+        super(x, y, KindOfGameObject.BONUS);
+        randomizeSprite();//a modif
+    }
 
-	public Candy() {
-		this.x=-1;
-		this.y=-1;
-	}
-	
-	public void addCandy(Sommet vertex) {
-		//Candy myCandy= new Candy();//(vertex.getX(), vertex.getY());
-		this.x=vertex.getX();
-		this.y=vertex.getY();
-	}
+    public Candy() {
+        super(KindOfGameObject.BONUS);
+        randomizeSprite();
+    }
 
+    public void randomizeSprite(){
+        Random random = TheDirector.getRandom();
+        ImageGameObject[] types = {ImageGameObject.CANDY1, ImageGameObject.CANDY2, ImageGameObject.CANDY3, ImageGameObject.CANDY4};
+        setImageGameObject(types[random.nextInt(4)]);
+    }
 
-	public int getX() {
-		return this.x;
-	}
-	public int getY() {
-		return this.y;
-	}
+    @Override
+    public void eventCollision() {
+    	TheDirector.addNbBonus();
+    	TheDirector.removeGameObject(this);
+    }
 }
+
